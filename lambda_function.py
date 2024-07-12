15ca425e3052b82d769ce6ad9207cfdf73edeac9
lchowdam <lakshmiramana.chowdam@cyberark.com>
2024-07-12 22:33:20 +0530

    lambda file

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import json
 import boto3
 import datetime
 import base64
+import random
+import string
 from boto3.dynamodb.conditions import Attr
 
 
@@ -11,22 +13,20 @@ current_time = datetime.datetime.now().isoformat()
 
 def lambda_handler(event, context):
     try:
-        decodedBytes = base64.b64decode(event['body'])
-        decodedStr = decodedBytes.decode("ascii")
-        payload = json.loads(decodedStr)
         print(f"query string parameters {event['queryStringParameters']}")
-        
         request_method = event['requestContext']['http']['method']
 
         if request_method == 'POST':
+            payload = decode_payload(event['body'])
             dynamodb_put_data(payload)     
             return {
                 'statusCode': 200,
                 'body': json.dumps('Item inserted/updated successfully in DynamoDB')
             }
         elif request_method == 'GET':
-            intentid = event['queryStringParameters']['intentid']
-            items = dynamo_table_scan(intentid)
+            intentid_param = event['queryStringParameters']['intentid']
+            intentid_filter_expression = Attr('intentTypeId').eq(intentid_param)
+            items = dynamo_table_scan(intentid_filter_expression)
             return {
                 'statusCode': 200,
                 'body': json.dumps(items)
@@ -38,9 +38,12 @@ def lambda_handler(event, context):
             'body': json.dumps(f'Error: {str(e)}')
         }
 
+def decode_payload(body):
+    decoded_bytes = base64.b64decode(body)
+    decoded_str = decoded_bytes.decode("ascii")
+    return json.loads(decoded_str)
 
-def dynamo_table_scan(intentid):
-    filter_expression = Attr('intentTypeId').eq(intentid)
+def dynamo_table_scan(filter_expression):
     get_response = table.scan(
                         FilterExpression=filter_expression
                     )
@@ -50,18 +53,22 @@ def dynamo_table_scan(intentid):
             FilterExpression=filter_expression,
             ExclusiveStartKey=response['LastEvaluatedKey']
         )
-        items.extend(response['Items'])
+        items.extend(response.get('Items', []))
     return items
 
 def dynamodb_put_data(payload):
     intent_type_id = payload.get('intentTypeId')
     intent_key = payload.get('key', {})
-    org_id = intent_key.get('orgId')
-    carrier_div = intent_key.get('carrierDiv')
-    plan_id = intent_key.get('planId')
-    state = intent_key.get('state')
+    org_id, carrier_div, plan_id, state = (
+        intent_key.get('orgId'),
+        intent_key.get('carrierDiv'),
+        intent_key.get('planId'),
+        intent_key.get('state')
+    )
+    intentid = get_existing_intentid(intent_type_id, org_id, carrier_div, plan_id, state) or generate_random_intentid()
+
     item = {
-            '_id': f"{intent_type_id}-{org_id}", 
+            'intentid': intentid, 
             'intentTypeId': intent_type_id,
             'key': {
                 'orgId': org_id,
@@ -73,16 +80,32 @@ def dynamodb_put_data(payload):
             'update_date': None
         }
     try:
-        item_key = {'_id': item['_id']}
-        existing_item = table.get_item(Key=item_key).get('Item')
+        existing_item = table.get_item(Key={'intentid': intentid}).get('Item')
         print(f"existing data : {existing_item}")
         if existing_item:
             item['update_date'] = current_time
             item['create_date'] = existing_item['create_date']
         else:
-            print(f"Item {item_key} not found in DynamoDB")
-
+            print(f"Item {intentid} not found in DynamoDB")
     except Exception as e:
         print(f"Error fetching item from DynamoDB: {str(e)}")
     put_response = table.put_item(Item=item)
     return put_response
+
+def generate_random_intentid():
+    intentid_length=24
+    characters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(intentid_length))
+
+def get_existing_intentid(intent_type_id, org_id, carrier_div, plan_id, state):
+    filter_expression = (
+        Attr('intentTypeId').eq(intent_type_id) &
+        Attr('key.orgId').eq(org_id) &
+        Attr('key.carrierDiv').eq(carrier_div) &
+        Attr('key.planId').eq(plan_id) &
+        Attr('key.state').eq(state)
+    )
+    items = dynamo_table_scan(filter_expression)
+    if items:
+        return items[0]['intentid']
+    return None
