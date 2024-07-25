221bdf9f66882a26562852f61a112961fa7e112f
lchowdam <lakshmiramana.chowdam@cyberark.com>
2024-07-26 01:41:09 +0530

    Revert "added fastapi"
    This reverts commit 13adae86470537797893bdaf9211bba1bd67b4bd.

--- lambda_function.py
+++ lambda_function.py
@@ -13,47 +13,22 @@ app = FastAPI()
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('bef_Intent_Api_Dev')
 current_time = datetime.datetime.now().isoformat()
-handler = Mangum(app)
 
 @app.get("/evernorth/v1/intent/intent_lambda/{intentid}")
-async def get_intent_data(intentid: str):
+async def get_intent_id(intentid: str):
     try:
-        print(f"Intent id : {intentid}")
         intentid_filter_expression = Attr('intentid').eq(intentid)
         items = dynamo_table_scan(intentid_filter_expression)
-        print(f"response items : {items}")
         if not items:
-            return {
-                'statusCode': 404,
-                'body': json.dumps(f'Item not found')
-            }
+            raise HTTPException(status_code=404, detail="Item not found")
         
         return {
             'statusCode': 200,
             'body': json.dumps(items)
         }
     except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f'Error: {str(e)}')
-        }
-    
-@app.post("/evernorth/v1/intent/intent_lambda")
-async def post_intent_data(data):
-    try:
-        print(f"post event data : {data}")
-        payload = decode_payload(data)
-        dynamodb_put_data(payload)
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Item inserted/updated successfully in DynamoDB')
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f'Error: {str(e)}')
-        }
-    
+        raise HTTPException(status_code=500, detail=str(e))
+
 def decode_payload(body):
     decoded_bytes = base64.b64decode(body)
     decoded_str = decoded_bytes.decode("ascii")
@@ -109,3 +84,20 @@ def generate_random_intentid():
     characters = string.ascii_lowercase + string.digits
     return ''.join(random.choice(characters) for _ in range(intentid_length))
 
+def lambda_handler(event, context):
+    try:
+        request = event['requestContext']['http']
+        if request['method'] == 'POST':
+            payload = decode_payload(event['body'])
+            dynamodb_put_data(payload)
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Item inserted/updated successfully in DynamoDB')
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error: {str(e)}')
+        }
+
+handler = Mangum(app)
