4080fd5da008dd9f76add2a5c1d3b481477f37cc
lchowdam <lakshmiramana.chowdam@cyberark.com>
2024-09-18 19:21:24 +0530

    added split repos

---
+++ split-repos/development-code/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {}
+}
+
+# used to get the account id
+data "aws_caller_identity" "current" {}
+
+
+# used to get the account id
+data "aws_caller_identity" "current" {}
+
+variable "lambda_functions_config_file" {
+  type    = string
+  default = "lambda_configuration.json"  
+}
+
+data "local_file" "lambda_config" {
+  filename = "${path.module}/${var.lambda_functions_config_file}"
+}
+
+locals {
+  lambda_functions = jsondecode(data.local_file.lambda_config.content)
+}
+
+# For each Lambda function, create a zip of the handler code
+data "archive_file" "lambda_zip" {
+  for_each = { for function in local.lambda_functions : function.function_name => function }
+
+  type        = "zip"
+  source_dir  = each.value.folder_path # Path to the folder containing Lambda handler code
+  output_path = "${path.module}/${each.value.function_name}.zip" # Output zip file path
+}
+
+# Upload the zip file to S3
+resource "aws_s3_object" "lambda_code" {
+  for_each    = { for function in local.lambda_functions : function.function_name => function }
+
+  bucket      = each.value.s3_bucket
+  key         = each.value.s3_key
+  source      = data.archive_file.lambda_zip[each.key].output_path # Reference the zipped file
+  etag   = filemd5(data.archive_file.lambda_zip[each.key].output_path)
+}
+
+resource "null_resource" "update_lambda_functions" {
+  provisioner "local-exec" {
+    command = "python ${path.module}/update_lambda_functions.py"
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  depends_on = [aws_s3_object.lambda_code]
+}
---
+++ split-repos/development-code/update_lambda_functions.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+
+# Load configuration from JSON file
+with open('lambda_configuration.json', 'r') as config_file:
+    functions = json.load(config_file)
+    
+aws_region = functions[0].get('region', '')
+
+
+# Initialize the S3 and Lambda clients
+s3_client = boto3.client('s3', region_name=aws_region)
+lambda_client = boto3.client('lambda', region_name=aws_region)
+
+# Process each Lambda function configuration
+for function in functions:
+    function_name = function['function_name']
+    s3_key = function['s3_key']
+    s3_bucket = function['s3_bucket']
+    
+    # Update Lambda function code
+    response = lambda_client.update_function_code(
+        FunctionName=function_name,
+        S3Bucket=s3_bucket,
+        S3Key=s3_key
+    )
+    print(f'Updated {function_name}: {response}')
